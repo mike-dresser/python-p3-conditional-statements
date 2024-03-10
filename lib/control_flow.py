2393f36a667ffce596cfba28b360437ab4bbec4a
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-10 15:38:23 -0400

    completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    response = 'Access granted' if (
+        password == '12345' and 
+        (username == 'admin' or username == 'ADMIN')
+     ) else 'Access denied'
+    return response
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature <  40: return "It's brisk out there!"
+    elif temperature < 65: return "It's a little chilly out there!"
+    elif temperature < 85: return "It's perfect out there!"
+    else: return "It's too dang hot out there!"
+    
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 15 == 0: return 'FizzBuzz'
+    if num % 5 == 0: return 'Buzz'
+    if num % 3 == 0: return 'Fizz'
+    return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    function_map = {
+        '+': lambda num1,num2: num1 + num2,
+        '-': lambda num1,num2: num1-num2,
+        '*': lambda num1,num2: num1* num2,
+        '/': lambda num1,num2: num1 / num2
+    }
+
+    def default_response(a,b):
+            print('Invalid operation!')
+            return None
+
+    return function_map.get(operation, default_response)(num1, num2)
